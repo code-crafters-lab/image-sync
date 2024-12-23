6cf566b12fdfdb6e846abb4fe4167b01efcb0478
Yujie Wu <coffee377@dingtalk.com>
2024-12-23 15:06:59 +0800

    :sparkles: Add image sync to Aliyun registry with credentials
    Signed-off-by: Yujie Wu <coffee377@dingtalk.com>

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	targetRegistryURL := os.Args[1]
+	targetIRegistryNamespace := os.Args[2]
+	dockerUsername := os.Args[3]
+	dockerPassword := os.Args[4]
+
+	fmt.Printf("Syncing images to %s/%s\n", targetRegistryURL, targetIRegistryNamespace)
+	fmt.Printf("Using Docker credentials for %s => %s\n", dockerUsername, dockerPassword)
 	fmt.Println("Image synced successfully")
 }
